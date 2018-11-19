d5727ac76d1e5a3e159de071a6031bb424724d04
VidadeDev <45027394+RodrigoFMuniz@users.noreply.github.com>
2018-11-19 13:58:33 -0200

    Add files via upload

---
+++ oslibrary.py
@@ -0,0 +1,8 @@
+import os
+
+print(os.name)#nome do tipo de sistema operacional. Os nomes previstos são: 'posix' (Linux, Mac), 'nt' (Windows), 'java'.
+print(os.getlogin())#  nome de usuário corrente.
+print(os.environ)#variáveis de ambiente do sistema.
+print(os.getcwd())#O comando os.getcwd() obtém o caminho (path) completo do diretório corrente.
+print(os.getpid())#O comando os.getpid() apresenta o PID (identificador de processo) do próprio processo em execução.
+
